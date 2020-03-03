577c344cc29ff095b83a5563b5194d29d6f679a1
Philippe <cottephi@gmail.com>
2020-03-03 12:47:54 +0100

    Removed useless print

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tablewriter",
-    version="0.0.1.4",
+    version="0.0.1.5",
     author="cottephi",
     author_email="cottephi@gmail.com",
     description="latex table generator",
