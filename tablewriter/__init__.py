dfd73c2946397dacdb4ed2d5bf3c09b50b5be860
pcotte <pcotte@advestis.com>
2022-04-14 18:02:56 +0200

    chg: CI/CD and versioneer

--- tablewriter/__init__.py
+++ tablewriter/__init__.py
@@ -1,6 +1,4 @@
 from .tablewriter import TableWriter
 
-try:
-    from ._version import __version__
-except ImportError:
-    pass
+from . import _version
+__version__ = _version.get_versions()['version']
